402b8f65171dd6cdb2899266130cdfcd3a1f7cbc
Vladimir Svistelnikov <svistelnikoff@gmail.com>
2019-07-20 18:56:33 +0300

    apps: add led controls to main activity app
    Signed-off-by: Vladimir Svistelnikov <svistelnikoff@gmail.com>

--- 03-Service/SetterApp/app/src/main/java/com/example/setterapp/MainActivity.java
+++ 03-Service/SetterApp/app/src/main/java/com/example/setterapp/MainActivity.java
@@ -6,28 +6,40 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
+import vendor.gl.ledcontrol.V1_0.ILedControl;
+import vendor.gl.ledcontrol.V1_0.Leds;
+import vendor.gl.ledcontrol.V1_0.LedState;
+
 public class MainActivity extends AppCompatActivity {
     private static String LOG_TAG = "SetterApp";
-    private com.example.setterapp.IGetSetServicelInterface mService;
     private BroadcastReceiver mBroadReceiver = null;
+    Switch mSwitchLed1 = null;
+    Switch mSwitchLed2 = null;
+    Switch mSwitchLed3 = null;
+    Switch mSwitchLed4 = null;
+    ILedControl ledControl = null;
 
-    private void Log(@NonNull String format, @NonNull Object... objects) {
-        Log.d(LOG_TAG, "-> %s" + String.format(format, objects));
+    private void Log(String message) {
+        Log.d(LOG_TAG, String.format(" -> %s", message));
     }
+
 
     @Override
     protected void onPostResume() {
-        Log("onPostResume()");
         super.onPostResume();
+        Log("onPostResume()");
 
         IntentFilter filter = new IntentFilter("com.example.getsetservice.SET_VALUE_RESULT");
         mBroadReceiver = new BroadcastReceiver() {
@@ -35,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             public void onReceive(Context context, Intent intent) {
                 if (intent.hasExtra("value")) {
                     int value = intent.getIntExtra("value", 0);
-                    Log("SetValue() method done: %d", value);
+                    Log(String.format("SetValue() method done: %d", value));
                 }
             }
         };
@@ -53,8 +65,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log("onCreate()");
         super.onCreate(savedInstanceState);
+        Log("onCreate()");
+
         setContentView(R.layout.activity_main);
 
         Button setButton = findViewById(R.id.setValueBtn);
@@ -64,6 +77,68 @@ public class MainActivity extends AppCompatActivity {
                 setValueBtnOnClick();
             }
         });
+
+        try {
+            ledControl = ILedControl.getService(true);
+        }
+        catch(RemoteException ex) {
+            Log(ex.toString());
+        }
+
+        mSwitchLed1 = findViewById(R.id.ledSwitch1);
+        mSwitchLed2 = findViewById(R.id.ledSwitch2);
+        mSwitchLed3 = findViewById(R.id.ledSwitch3);
+        mSwitchLed4 = findViewById(R.id.ledSwitch4);
+
+        mSwitchLed1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                try {
+                    Log(String.format("LED1 -> %s", isChecked ? "enabled" : "disabled"));
+                    ledControl.setLedState(Leds.LED_1, isChecked ? LedState.LED_STATE_ON : LedState.LED_STATE_OFF);
+                }
+                catch(RemoteException ex) {
+                    Log(ex.toString());
+                }
+            }
+        });
+        mSwitchLed2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                try {
+                    Log(String.format("LED2 -> %s", isChecked ? "enabled" : "disabled"));
+                    ledControl.setLedState(Leds.LED_2, isChecked ? LedState.LED_STATE_ON : LedState.LED_STATE_OFF);
+                }
+                catch (RemoteException ex) {
+                    Log(ex.toString());
+                }
+            }
+        });
+        mSwitchLed3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                try {
+                    Log(String.format("LED3 -> %s", isChecked ? "enabled" : "disabled"));
+                    ledControl.setLedState(Leds.LED_3, isChecked ? LedState.LED_STATE_ON : LedState.LED_STATE_OFF);
+                }
+                catch (RemoteException ex) {
+                    Log(ex.toString());
+                }
+            }
+        });
+        mSwitchLed4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                try {
+                    Log(String.format("LED4 -> %s", isChecked ? "enabled" : "disabled"));
+                    ledControl.setLedState(Leds.LED_4, isChecked ? LedState.LED_STATE_ON : LedState.LED_STATE_OFF);
+                }
+                catch (RemoteException ex) {
+                    Log(ex.toString());
+                }
+            }
+        });
+
     }
 
     private void setValueBtnOnClick() {
@@ -74,5 +149,9 @@ public class MainActivity extends AppCompatActivity {
         intent.setExtrasClassLoader(getClassLoader());
         intent.putExtra("value", new Random().nextInt(100));
         startForegroundService(intent);
+    }
+
+    private void sendActionWithExtra(String actionName, String extraName, Object extraValue) {
+        Log("sendActionWithExtra()");
     }
 }
