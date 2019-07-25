d995372b732865b96eacfc2e44e9c0ead309ef4f
Vladimir Svistelnikov <svistelnikoff@gmail.com>
2019-07-25 20:18:28 +0300

    apps: getter app fixes
    Signed-off-by: Vladimir Svistelnikov <svistelnikoff@gmail.com>

--- 03-Service/GetterApp/Android.mk
+++ 03-Service/GetterApp/Android.mk
@@ -7,16 +7,23 @@ LOCAL_USE_AAPT2 := true
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main/java)
-LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main/aidl)
+LOCAL_SRC_FILES += $(call all-Iaidl-files-under, app/src/main/aidl)
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
 
 LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
 
+#LOCAL_STATIC_ANDROID_LIBRARIES := \
+#	androidx.appcompat_appcompat \
+#	androidx-constraintlayout_constraintlayout
+
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-	androidx.appcompat_appcompat \
-	androidx-constraintlayout_constraintlayout
-	
+    android-support-v7-appcompat \
+    android-support-constraint-layout
+
+LOCAL_STATIC_JAVA_LIBRARIES += \
+    android-support-constraint-layout-solver
+
 LOCAL_PACKAGE_NAME := GetterApp
 
 LOCAL_SDK_VERSION := 28
--- 03-Service/GetterApp/app/src/main/java/com/example/getterapp/MainActivity.java
+++ 03-Service/GetterApp/app/src/main/java/com/example/getterapp/MainActivity.java
@@ -10,8 +10,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+//import androidx.annotation.NonNull;
+import android.support.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static String LOG_TAG = "GetterApp";
